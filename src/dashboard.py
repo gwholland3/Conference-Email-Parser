0485f355a3aa20b5b0c3ec038610bfc2d418d23e
Grant Holland <gwholland3@gmail.com>
2023-03-23 14:11:51 -0700

    Add comments to dashboard Python file

--- src/dashboard.py
+++ src/dashboard.py
@@ -31,8 +31,10 @@ def add_to_dashboard(email_summaries, append=False):
     append=True appends the new parsed emails following the existing ones.
     """
 
+    # Get a reference to the soup of our dashboard HTML and its table element
     soup, table = get_table(append)
 
+    # Add table header if it doesn't exist (which will happend when not appending)
     if table.thead.tr is None:
         header_row = soup.new_tag('tr')
         table.thead.append(header_row)
@@ -41,20 +43,31 @@ def add_to_dashboard(email_summaries, append=False):
             header_row.append(th)
             th.append(supported_fields[field])
         
+    # Add a table row for each email
     for parsed_email in email_summaries:
         email_row = soup.new_tag('tr')
         table.tbody.append(email_row)
         for field in supported_fields:
             td = soup.new_tag('td')
             email_row.append(td)
+            # If this email has an actual value for current column, use it, otherwise display "N/A"
             content = parsed_email[field] if field in parsed_email and parsed_email[field] is not None  else "N/A"
             td.append(content)
 
+    # Write the modified HTML containing our email summaries back to the dashboard HTML file
     with open(DASHBOARD_FILEPATH, 'w') as f:
         f.write(soup.prettify())
 
 
 def get_table(append):
+    """
+    Finds and returns references to both the table element in our dashboard HTML
+    and the root soup of the document.
+
+    If appending, loads in the current dashboard and looks for it there.
+    Otherwise, creates a new dashboard with our template and finds the template's table element
+    """
+
     if append:
         try:
             with open(DASHBOARD_FILEPATH) as f:
@@ -66,6 +79,7 @@ def get_table(append):
         except:
             print("Couldn't find existing index.html, generating new file")
 
+    # Generate new dashboard if not appending, current dashboard doesn't exist, or current dashboard doesn't have a table element
     with open(TEMPLATE_FILEPATH) as f:
         soup = BeautifulSoup(f, 'html.parser')
     table = soup.find('table')
@@ -76,4 +90,4 @@ def get_table(append):
 # Test script
 if __name__ == '__main__':
     email_summaries = [{'source_name': 'email1', 'conf_name': 'conf1'}, {'source_name': 'email2'}]
-    add_to_dashboard(email_summaries, append=True)
+    add_to_dashboard(email_summaries)
